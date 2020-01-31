f432229a31b2ce0fe9e49d789a0a59a40acadbeb
louib <L0U13@protonmail.com>
2020-01-30 19:42:56 -0500

    Almost there

--- app/src/main/java/com/example/nwipe_android/MainActivity.java
+++ app/src/main/java/com/example/nwipe_android/MainActivity.java
@@ -1,14 +1,10 @@
 package com.example.nwipe_android;
 
-import android.app.usage.StorageStatsManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.BatteryManager;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -16,24 +12,23 @@ import androidx.appcompat.widget.Toolbar;
 import android.os.Environment;
 import android.os.MemoryFile;
 import android.os.StatFs;
-import android.os.storage.StorageManager;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Random;
-import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static int WIPE_BUFFER_SIZE = 2048;
+    public static int WIPE_BUFFER_SIZE = 4096;
 
     private boolean isWiping = false;
     @Override
@@ -75,18 +70,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startWipe(View v) {
-        // mainTextView = (TextView) findViewById(R.id.main_text_view);
-        // startWipeButton = (Button) findViewById(R.id.start_wipe_button);
+        Button startWipeButton = (Button) findViewById(R.id.start_wipe_button);
         if (this.isWiping) {
-            Log.i("MainActivity", "Cancelling wipe process.");
-            // mainTextView.setText("temp");
-            // startWipeButton.setText("Start Wipe");
-            this.isWiping = false;
+            Log.i("MainActivity", "Cancelling startWipe process.");
+            this.stopWipe();
         } else {
-            Log.i("MainActivity", "Starting wipe process.");
-            // mainTextView.setText("temp");
-            // startWipeButton.setText("Wiping...");
+            Log.i("MainActivity", "Starting startWipe process.");
+            startWipeButton.setText("Cancel startWipe");
             this.isWiping = true;
+            try {
+                this.startWipe();
+            } catch (IOException e) {
+                Log.i("MainActivity", "Could not startWipe device." + e.getMessage());
+            }
         }
     }
 
@@ -103,32 +99,57 @@ public class MainActivity extends AppCompatActivity {
         return status == BatteryManager.BATTERY_STATUS_CHARGING || status == BatteryManager.BATTERY_STATUS_FULL;
     }
 
-    public void wipe() throws IOException {
+    public void startWipe() throws IOException {
+        Context context = getApplicationContext();
+
         long availableBytesCount = MainActivity.getAvailableBytesCount();
+        int availableBytesCountCasted = (int)availableBytesCount;
+        if (availableBytesCountCasted <= 0) {
+            availableBytesCountCasted = Integer.MAX_VALUE;
+        }
+        Log.i("MainActivity", String.format("Got %d bytes available for writing.", availableBytesCount));
+
+        ProgressBar wipeProgressBar = (ProgressBar) findViewById(R.id.wipe_progress_bar);
+        wipeProgressBar.setVisibility(View.VISIBLE);
+
+
+        wipeProgressBar.setMax(availableBytesCountCasted);
+
 
         // TODO handle the int/long cast.
-        MemoryFile wipeFile = new MemoryFile("nwipe-android-timestamp", (int)availableBytesCount);
-        try {
-            // This method is deprecated in API level 29, so it might raise.
-            wipeFile.allowPurging(false);
-        } catch (IOException e) {
-            Log.i("MainActivity", "Could not set wipe file to non purgeable.");
-        }
+        // TODO add real timestamp.
+        String wipeFileName = String.format("nwipe-android-%d", System.currentTimeMillis());
+        //File file = new File(context.getFilesDir(), wipeFileName);
 
         Random rnd = new Random();
         // TODO get an actual random seed.
         rnd.setSeed(1);
         byte[] bytesBuffer = new byte[WIPE_BUFFER_SIZE];
 
-        OutputStream outputStream = wipeFile.getOutputStream();
-        int writtenBytesCount = 0;
-        while (writtenBytesCount < availableBytesCount) {
-            int bytesToWriteCount = Math.min(WIPE_BUFFER_SIZE, ((int)availableBytesCount - writtenBytesCount));
-            rnd.nextBytes(bytesBuffer);
-            outputStream.write(bytesBuffer, writtenBytesCount, bytesToWriteCount);
+        try (FileOutputStream fos = context.openFileOutput(wipeFileName, Context.MODE_PRIVATE)) {
+            int writtenBytesCount = 0;
+            while (writtenBytesCount < availableBytesCount) {
 
-            writtenBytesCount += bytesToWriteCount;
+
+                int bytesToWriteCount = Math.min(WIPE_BUFFER_SIZE, (availableBytesCountCasted - writtenBytesCount));
+                rnd.nextBytes(bytesBuffer);
+                fos.write(bytesBuffer, 0, bytesToWriteCount);
+
+                writtenBytesCount += bytesToWriteCount;
+                wipeProgressBar.setProgress(writtenBytesCount);
+
+            }
         }
+
+    }
+
+    public void stopWipe() {
+        Button startWipeButton = (Button) findViewById(R.id.start_wipe_button);
+        ProgressBar wipeProgressBar = (ProgressBar) findViewById(R.id.wipe_progress_bar);
+
+        wipeProgressBar.setVisibility(View.INVISIBLE);
+        startWipeButton.setText("Start startWipe");
+        this.isWiping = false;
     }
 
     /*
