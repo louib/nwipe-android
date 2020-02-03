7971cb53ffb7b2239bc99cb6b9ebd1cca588c974
louib <L0U13@protonmail.com>
2020-02-03 15:26:58 -0500

    WipeAsyncTask template.

---
+++ app/src/main/java/com/example/nwipe_android/WipeAsyncTask.java
@@ -0,0 +1,11 @@
+package com.example.nwipe_android;
+
+import android.os.AsyncTask;
+
+public class WipeAsyncTask extends AsyncTask <String, Void, Void> {
+
+    @Override
+    protected Void doInBackground(String... strings) {
+        return null;
+    }
+}
