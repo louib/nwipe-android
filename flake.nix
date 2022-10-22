5f32b9e17473e152cf2909030f95969d8704483d
louib <code@louib.net>
2022-10-22 18:44:36 -0400

    Progress on having a working flake

--- flake.nix
+++ flake.nix
@@ -2,30 +2,58 @@
   description = "Nix flake for nwipe-android development";
 
   inputs = {
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
     nixpkgs = {
       url = "github:NixOS/nixpkgs";
     };
-    flake-utils = {
-      url = "github:numtide/flake-utils";
+    android-nixpkgs = {
+      url = "github:tadfisher/android-nixpkgs";
+
+      # The main branch follows the "canary" channel of the Android SDK
+      # repository. Use another android-nixpkgs branch to explicitly
+      # track an SDK release channel.
+      #
+      # url = "github:tadfisher/android-nixpkgs/stable";
+      # url = "github:tadfisher/android-nixpkgs/beta";
+      # url = "github:tadfisher/android-nixpkgs/preview";
+      # url = "github:tadfisher/android-nixpkgs/canary";
+
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
     };
   };
 
   outputs = {
     self,
     nixpkgs,
+    android-nixpkgs,
     flake-utils,
   }: (
     flake-utils.lib.eachDefaultSystem (
       system: (
         let
           pkgs = nixpkgs.legacyPackages.${system};
-          devPkgs = with pkgs; [
-            gradle
+          devPkgs = [
+            pkgs.gradle
           ];
+          androidPackage = (
+            android-nixpkgs.sdk.${system} (
+              sdkPkgs:
+                with sdkPkgs; [
+                  cmdline-tools-latest
+                  build-tools-32-0-0
+                  platform-tools
+                  platforms-android-31
+                  emulator
+                ]
+            )
+          );
         in {
           devShell = pkgs.mkShell {
-            buildInputs = devPkgs;
-            nativeBuildInputs = devPkgs;
+            buildInputs = devPkgs ++ [androidPackage];
+            nativeBuildInputs = devPkgs ++ [androidPackage];
           };
           /*
           defaultPackage = pkgs.androidPlatform.buildAndroidPackage {
